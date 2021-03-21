ce56b172bce97ea394f2078ce2d19ce32085e930
Glitch (gratis-blue-cocoa) <none>
2021-03-21 12:59:39 +0000

    😘🐡 Checkpoint ./server.js:29834375/3223 ./package.json:29834375/883

--- server.js
+++ server.js
@@ -1,59 +1,71 @@
 //require express and bodyParser
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //create express app
 const app = express();
 
 //port
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 1000;
 
 //use bodyParser middleware on express app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors())
 
 
 //Databases Station
-
-
 const url =
-  "mongodb+srv://vutrantienbao290699:vutrantienbao99@project.murnk.mongodb.net/testretryWrites=true&w=majority";
+  "mongodb+srv://vutrantienbao290699:vutrantienbao99@project.murnk.mongodb.net/test?retryWrites=true&w=majority";
 const { MongoClient } = require("mongodb");
-const client = new MongoClient(url);
-client.connect(function (err) {
-  if (err) throw err;
-  //Main page route
-  app.route("/").get((req, res) => {
-    res.status(200).send({
-      listUsersRoute: {
-        Name: "Get All Datas",
-        Description: "Return all users and passwords wifi",
-        Path: "/GetAllStationOne (Two,...)",
-      },
-    });
-  });
-  //Station route
-  const db = client.db("test");
 
-  //StationOne Get all data
-  let collection = db.collection("Tram_1");
-  collection.find({}).toArray((err, result) => {
-    app.route("/GetAllStationOne").get((req, res) => {
+
+
+app.route("/").get((req, res) => {
+
+  res.status(200).send({
+    listUsersRoute: {
+      Name: "Get All Datas",
+      Description: "Return all users and passwords wifi",
+      Path: "/GetAllStationOne (Two,...)",
+    },
+  });
+});
+
+
+app.route("/station/list/:id").get((req, res) => {
+  const client = new MongoClient(url);
+  client.connect(function (err) {
+    if (err) throw err;
+
+    const db = client.db("test");
+    const collection = db.collection("Tram_" + req.params.id);
+    console.log("Tram_" + req.params.id);
+    collection.find({}).toArray((err, result) => {
+      if (err) { console.log(err) };
       res.status(200).send(result);
-    });
+    })
   });
-
-  //StationOne get
-  collection.find({}).sort({ $natural: -1 }).limit(1).toArray((err, result) => {
-    app.route("/APIStationOne").get((req, res) => { res.status(200).send(result); });
-
-  });
-
-
-
-  //Listen to server
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+  client.close();
 })
+
+app.route("/station/:id").get((req, res) => {
+  const client = new MongoClient(url);
+  client.connect(function (err) {
+    if (err) throw err;
+    const db = client.db("test");
+    const collection = db.collection("Tram_" + req.params.id);
+    collection.find({}).sort({ $natural: -1 })
+      .limit(1)
+      .toArray((err, result) => {
+        res.status(200).send(result);
+      })
+  });
+  client.close();
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
