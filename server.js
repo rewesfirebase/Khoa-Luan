c18a9c9714062a571a59959688facc5bc67251b3
Glitch (gratis-blue-cocoa) <none>
2021-04-07 16:07:37 +0000

    🐃⏰ Checkpoint ./server.js:29834375/3654

--- server.js
+++ server.js
@@ -12,63 +12,60 @@ const port = process.env.PORT || 1000;
 //use bodyParser middleware on express app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 const { MongoClient } = require("mongodb");
 //Databases Station
 const url =
   "mongodb+srv://vutrantienbao290699:vutrantienbao99@project.murnk.mongodb.net/test?retryWrites=true&w=majority";
 
-
-
-
 app.route("/").get((req, res) => {
-
   res.status(200).send({
-    
     "Get All Datas": {
       Description: "Return all data",
-      Path: "/station/list/1 (2, 3,...)",
+      Path: "/station/list/1 (2, 3,...)"
     },
-    "API applications":{
+    "API applications": {
       Description: "Return recent data",
-      Path: "/station/1 (2, 3,...)",
+      Path: "/station/1 (2, 3,...)"
     }
   });
 });
 
-
 app.route("/station/list/:id").get((req, res) => {
   const client = new MongoClient(url);
-  client.connect(function (err) {
+  client.connect(function(err) {
     if (err) throw err;
-    
+
     const db = client.db("test");
     const collection = db.collection("Tram_" + req.params.id);
     console.log("Tram_" + req.params.id);
     collection.find({}).toArray((err, result) => {
-      if (err) { console.log(err) };
+      if (err) {
+        console.log(err);
+      }
       res.status(200).send(result);
-    })
+    });
   });
   client.close();
-})
+});
 
 app.route("/station/:id").get((req, res) => {
   const client = new MongoClient(url);
-  client.connect(function (err) {
+  client.connect(function(err) {
     if (err) throw err;
     const db = client.db("test");
     const collection = db.collection("Tram_" + req.params.id);
-    collection.find({}).sort({ $natural: -1 })
+    collection
+      .find({})
+      .sort({ $natural: -1 })
       .limit(1)
       .toArray((err, result) => {
         res.status(200).send(result);
-      })
+      });
   });
   client.close();
-})
-
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
